f4acdab4aff2de30f1ccfd2ac362e27e514c4a95
adowns3 <adowns3@users.noreply.github.com>
2019-08-29 13:15:21 -0500

    Create hello.c
    add the hello.c file

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Aaron Downs
+ * Date: 8/29/2019
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+
+  return 0;
+}
